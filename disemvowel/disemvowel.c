4f021302014f5ef402420f0a8e06c4680048f775
michael-small <small203@morris.umn.edu>
2019-09-24 12:24:50 -0500

    Allocate the right memeory space by character amount in disemvowel \n \n Not working overall, as in not taking out vowels

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,62 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+
+int nonVowelLengthAllocater(char const *str) {
+    const char *invalid_chars = "AEIOUaeiou";
+    char mystring[] = "This is my test string"; //mystring[]
+    char *c = mystring;
+    int vowel_count = 0;
+  
+    while(*c){
+        if(strchr(invalid_chars, *c)){
+            vowel_count++;
+        }
+        c++;
+    }
+
+    printf("Vowel count: %d\n", vowel_count);
+
+    printf("Amount of nonvowels: %lu\n", strlen(mystring) - vowel_count);
+
+    int len = strlen(mystring) - vowel_count;
+  
+    return len;
+}
+
+char *disemvowel(char const *str){
+    const char *invalid_chars = "AEIOUaeiou";
+    int length, i; 
+    char *result;
+    char const *c = str;
+
+    length = nonVowelLengthAllocater(str);
+    result = (char*) calloc(length+1, sizeof(char));
+
+    for(i = 0; i < length; ++i){
+        if(!strchr(invalid_chars, *str)){
+            result[i] = str[i];
+            c++;
+        }
+    }
+
+    result[length] = '\0';
+
+    printf("Disemvowel end result: %s\n", result);
+
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    char const the_string[] = "This is my test string";
+    char *r;
+
+    r = disemvowel(the_string);
+    printf("Main returns: %s\n", r);
+    free(r);
 }
