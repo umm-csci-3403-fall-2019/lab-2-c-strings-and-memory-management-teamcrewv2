2619428e58c4885372209a7102469835d8483cf3
michael-small <small203@morris.umn.edu>
2019-09-25 12:04:21 -0500

    Fix placing of valid chars in result in hardcoded test sequence

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -7,66 +7,47 @@
 #include "disemvowel.h"
 
 
-int nonVowelLengthAllocater(char const *str) {
+char *disemvowel(char const *str) {
     const char *invalid_chars = "AEIOUaeiou";
-    char mystring[] = "This is my test string"; //mystring[]
-    char *c = mystring;
+    char mystring[] = "Test test dog"; //mystring[]
+    char *str_copy = mystring;
     int vowel_count = 0;
 
-    int i;
-    i = 0;
+    int i = 0;
+    int j = 0;
   
-    while(*c){
-        if(strchr(invalid_chars, *c)){
+    while(*str_copy){
+        if(strchr(invalid_chars, *str_copy)){
             vowel_count++;
-            // printf("%c\n", mystring[i]);
         }
-        c++;
-        i++;
+        str_copy++;
     }
-
-    printf("Vowel count: %d\n", vowel_count);
-
-    printf("Amount of nonvowels: %lu\n", strlen(mystring) - vowel_count);
 
     int len = strlen(mystring) - vowel_count;
-  
-    return len;
-}
 
-char *disemvowel(char const *str){
-    const char *invalid_chars = "AEIOUaeiou";
-    int length, i; 
-    char *result;
-    char const *c = str;
+    char *result = (char*) calloc(len+1, sizeof(char));
 
-    i = 0;
-
-    length = nonVowelLengthAllocater(str);
-    printf("Length: %lu\n", length);
-    result = (char*) calloc(length+1+1+5, sizeof(char));
-
-    while(*c){
-        if(!strchr(invalid_chars, *c)){
-            result[i] = str[i];
-            printf("%c\n", str[i]);
-        }
-        c++;
-        i++;
+    for(i=0; i < strlen(mystring); i++){
+        if(!strchr(invalid_chars, str[i])){
+            result[j] = str[i];
+            j++;
+        } 
     }
 
-    result[length] = '\0';
+    result[len] = '\0';
 
-    printf("Disemvowel end result: %s\n", result);
-
+    printf(result);
+  
     return result;
 }
 
 int main(int argc, char *argv[]) {
-    char const the_string[] = "This is my test string";
-    char *r;
+    char const the_string[] = "Test test dog";
+    char *t;
 
-    r = disemvowel(the_string);
-    printf("Main returns: %s\n", r);
-    free(r);
+    t = disemvowel(the_string);
+
+    free(t);
+
 }
+
